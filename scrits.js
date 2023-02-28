d5e000db6487aa9d05684d9b03a363396071a941
brspkts <122319070+brspkts@users.noreply.github.com>
2023-02-28 17:26:24 +0300

    Add files via upload

---
+++ scrits.js
@@ -0,0 +1,8 @@
+
+
+function showSpend1(){
+    document.getElementById("price1").style.display = "block";
+}
+function showSpend2(){
+    document.getElementById("price2").style.display = "block";
+}
